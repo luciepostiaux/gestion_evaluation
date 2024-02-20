b1930972587549d9d661ef5f420d520b752f66aa
verwaerdeaurore <aurore.verwaerde@ifosup.wavre.be>
2024-02-20 18:00:45 +0100

    front-end des formulaires d'ajout

--- resources/js/Components/FormStudent.vue
+++ resources/js/Components/FormStudent.vue
@@ -27,104 +27,90 @@ const form = useForm({
 const submitStudent = () => {
     form.post(route("students.store"), {
         onSuccess: () => {
+            form.name = null;
+            form.section_id = null;
             close();
         },
     });
 };
 </script>
 <template>
-    <div
-        v-show="show"
-        class="mb-6 rounded-lg overflow-hidden transform transition-all sm:w-full sm:mx-auto"
-    >
-        <!-- Bouton de fermeture en haut à droite -->
-        <div class="flex justify-end p-2">
-            <button @click="close" class="text-gray-400 hover:text-gray-600">
-                <span class="text-2xl">&times;</span>
-            </button>
-        </div>
-        <slot v-if="show" />
-    </div>
-
     <div class="px-6 py-4">
-        <form
-            class="mt-4 text-sm text-gray-600"
-            @submit.prevent="submitStudent"
-        >
-            <h1 class="text-lg font-bold mb-4">Ajouter un(e) élève</h1>
-
-            <!-- Champ pour le nom -->
-            <div class="mb-4">
-                <label
-                    for="lastname"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                >
-                    Nom de famille
-                </label>
-                <input
-                    type="text"
-                    id="lastname"
-                    name="lastname"
-                    required
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="Entrez le nom de famille de l'élève"
-                    v-model="form.lastname"
-                />
-            </div>
-            <div class="mb-4">
-                <label
-                    for="firstname"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                >
-                    Prénom
-                </label>
-                <input
-                    type="text"
-                    id="firstname"
-                    name="firstname"
-                    required
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    placeholder="Entrez le nom du cours"
-                    v-model="form.firstname"
-                />
-            </div>
-
-            <div class="mb-4">
-                <label
-                    for="section"
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                >
-                    Associer à une section
-                </label>
-                <select
-                    id="section"
-                    name="section_id"
-                    required
-                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    v-model="form.section_id"
-                >
-                    <option type="" value="" disabled>
-                        Sélectionnez une section
-                    </option>
-                    <option
-                        v-for="section in props.sections"
-                        :key="section.id"
-                        :value="section.id"
+        <div class="flex justify-center items-center">
+            <form
+                class="bg-white shadow sm:rounded-md p-8 text-sm text-gray-600 w-full max-w-xl"
+                @submit.prevent="submitStudent"
+            >
+                <div class="mb-4">
+                    <label
+                        for="lastname"
+                        class="block text-[#1F2D55] font-poppins text-base font-bold mb-2"
                     >
-                        {{ section.name }}
-                    </option>
-                </select>
-            </div>
+                        Nom de famille
+                    </label>
+                    <input
+                        type="text"
+                        id="lastname"
+                        name="lastname"
+                        required
+                        class="shadow border-[#62BFC1] rounded w-full mb-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-[#62BFC1]"
+                        placeholder="Entrez le nom de famille de l'élève"
+                        v-model="form.lastname"
+                    />
+                </div>
+                <div class="mb-4">
+                    <label
+                        for="firstname"
+                        class="block text-[#1F2D55] font-poppins text-base font-bold mb-2"
+                    >
+                        Prénom
+                    </label>
+                    <input
+                        type="text"
+                        id="firstname"
+                        name="firstname"
+                        required
+                        class="shadow border-[#62BFC1] rounded w-full mb-2 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline focus:border-[#62BFC1]"
+                        placeholder="Entrez le prénom de l'élève"
+                        v-model="form.firstname"
+                    />
+                </div>
 
-            <!-- Bouton pour soumettre le formulaire -->
-            <div class="flex items-center justify-between">
-                <button
-                    type="submit"
-                    class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                    Ajouter
-                </button>
-            </div>
-        </form>
+                <div class="mb-4">
+                    <label
+                        for="section"
+                        class="block text-[#1F2D55] font-poppins text-base font-bold mb-2"
+                    >
+                        Associer à une section
+                    </label>
+                    <select
+                        id="section"
+                        name="section_id"
+                        required
+                        class="shadow appearance-none border-[#62BFC1] rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring-[#62BFC1] focus:shadow-outline focus:border-[#62BFC1]"
+                        v-model="form.section_id"
+                    >
+                        <option type="" value="" disabled>
+                            Sélectionnez une section
+                        </option>
+                        <option
+                            v-for="section in props.sections"
+                            :key="section.id"
+                            :value="section.id"
+                        >
+                            {{ section.name }}
+                        </option>
+                    </select>
+                </div>
+                <div class="flex justify-end">
+                    <button
+                        type="submit"
+                        class="bg-[#62BFC1] font-poppins text-white text-base font-bold py-2 px-6 rounded"
+                    >
+                        Ajouter
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
