bd3448ac95372ce60a98218e2a0e491e2a998d2d
verwaerdeaurore <aurore.verwaerde@ifosup.wavre.be>
2024-02-28 12:03:41 +0100

    front-end liste section

--- resources/js/Pages/Sections/Index.vue
+++ resources/js/Pages/Sections/Index.vue
@@ -37,7 +37,7 @@ const deleteSection = (sectionId) => {
     if (confirm("Êtes-vous sûr de vouloir supprimer cette section ?")) {
         Inertia.delete(route("sections.destroy", sectionId), {
             preserveScroll: true,
-            preserveState: false, // Vous pouvez essayer de changer cela si nécessaire
+            preserveState: false,
             onSuccess: () => {
                 props.sections = props.sections.filter(
                     (section) => section.id !== sectionId
@@ -86,49 +86,51 @@ const deleteSection = (sectionId) => {
                         <input
                             v-model="form.name"
                             type="text"
-                            class="input"
+                            class="input rounded border-[#62BFC1]"
                             placeholder="Nom de la section"
                         />
                         <button
                             @click="saveSection(section.id)"
-                            class="btn font-semibold text-xs"
+                            class="btn font-semibold text-xl text-white ml-4 px-4 py-1.5 rounded bg-[#62BFC1]"
                         >
-                            Sauvegarder
+                            ✓
                         </button>
                         <button
                             @click="stopEditing"
-                            class="btn font-semibold text-xs"
+                            class="btn font-semibold text-xl text-white bg-red-600 px-5 py-1.5 rounded ml-2"
                         >
-                            Annuler
+                            ×
                         </button>
                     </div>
-                    <div v-else>
+                    <div
+                        v-else
+                        class="flex items-center justify-between w-full"
+                    >
                         <Link
                             :href="route('sections.lessons.index', section.id)"
                             class="w-full text-center"
+                            >{{ section.name }}</Link
                         >
-                            {{ section.name }}
-                        </Link>
-                        <button
-                            @click="startEditing(section)"
-                            class="btn font-semibold text-xl text-white px-4 py-1 rounded-sm bg-[#62BFC1]"
-                        >
-                            ✎
-                        </button>
-                    </div>
-                    <div class="flex space-x-2">
-                        <DangerButton @click="deleteSection(section.id)">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24"
-                                fill="#FFFFFF"
-                                class="size-6"
+                        <div class="flex space-x-2 items-center">
+                            <button
+                                @click="startEditing(section)"
+                                class="btn font-semibold text-xl text-white px-4 py-1.5 rounded bg-[#62BFC1]"
                             >
-                                <path
-                                    d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"
-                                ></path>
-                            </svg>
-                        </DangerButton>
+                                ✎
+                            </button>
+                            <DangerButton @click="deleteSection(section.id)">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="#FFFFFF"
+                                    class="size-6"
+                                >
+                                    <path
+                                        d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM18 8H6V20H18V8ZM9 11H11V17H9V11ZM13 11H15V17H13V11ZM9 4V6H15V4H9Z"
+                                    ></path>
+                                </svg>
+                            </DangerButton>
+                        </div>
                     </div>
                 </li>
             </ul>
